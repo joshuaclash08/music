c781caa921e8bf0e648d8cfd97683e27ee61b3be
joshy <61219866+joshuaclash08@users.noreply.github.com>
2022-09-19 00:25:31 +0900

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-const xml = new XMLHttpRequest();
-xml.open("GET","https://api.ipify.org");
-xml.send();
-xml.addEventListener("loadend",ipify);
-const agent = window.navigator.userAgent.toLowerCase();
-function ipify(e){here(xml.responseText);}
-function here(ip){console.log(ip),console.log(agent)}
